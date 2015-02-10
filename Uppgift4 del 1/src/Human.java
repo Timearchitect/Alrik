5016d9460556ad36c1529741d0fd8a468816f114
timearchitect <alrik_he@hotmail.com>
2015-02-10 17:59:01 +0100

    classes

---
+++ Uppgift4 del 1/src/Human.java
@@ -0,0 +1,22 @@
+public class Human {
+	private Dog dog;
+	private String name;
+	
+	public Dog getDog() {
+		return dog;
+	}
+
+	public void buyDog(Dog dog) {
+		this.dog = dog;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
